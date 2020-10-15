c38a6d96604c354623b17b1a78bd41930da299b9
Sebghat Yusuf <sebghatyusuf@gmail.com>
2020-10-15 07:45:51 +0430

    complete test_email

--- login.py
+++ login.py
@@ -1,6 +1,17 @@
+import re
+import pytest
+
+
 def check_email_format(email):
     """check that entered email format is correct."""
-    pass
+    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        raise Exception("Invalid email format")
+    else:
+        return "Email format is OK"
 
+
+# Using pytest.raises in a with block as a context manager, we can check that an exception is actually raised if an invalid email is given. Running the tests on the code as it is above should fail:
 def test_email_exception():
     """test that exception is raised for invalid email"""
+    with pytest.raises(Exception):
+        assert check_email_format("fine@gmail.com")
